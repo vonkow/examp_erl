d9bd37d88b065a34aca792242d00052187a15be6
Caz vonKow <skopsycats@gmail.com>
2011-03-31 19:19:49 -0400

    fixed

--- examp5.erl
+++ examp5.erl
@@ -1,9 +1,5 @@
 -module(examp5).
 -export([map/2]).
 
-map(F, L) -> map(F, L, []).
-
-map(_, [], Acc) ->
-	lists:reverse(Acc);
-map(F, [H|T], Acc) ->
-	map(F, T, [F(H) | Acc]).
+map(F, [H|T]) -> [F(H)|map(F, T)];
+map(F, []) -> [].
